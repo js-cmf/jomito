d4e16c3ac32970300301cca63b74a3d8d4d7e463
Todd Chisom <hello@toddchisom.me>
2016-10-04 16:38:27 -0700

    Added values to grab from the post

--- client/post.js
+++ client/post.js
@@ -6,14 +6,14 @@
 	
 	postData.onsubmit = (e) => {
 		e.preventDefault();
-		const title = document.getElementById().value;
-		const body = document.getElementById().value;
-		const status = document.getElementById().value;
-		const type = document.getElementById().value;
-		const user = document.getElementById().value;
-		const imgUrl = document.getElementById().value;
-		const videoEmbed = document.getElementById().value;
-		const publishDate =
+		const title = document.getElementById('post-title').value;
+		const body = document.getElementById('post-body').value;
+		const status = document.getElementById('post-status-display').textContent;
+		const type = document.getElementById('post_type').value;
+		const user = 'Mario';
+		const imgUrl = [];
+		const videoEmbed = [];
+		// const publishDate = document.getElementById().value;
 
 	}
 
