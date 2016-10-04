3ce9dbde4dfbf474561c5c6ec8bc110f91d02782
Mike McCready <mikem@inspero.net>
2016-10-04 14:32:20 -0700

    add ajax requests for login and signup

--- client/components/login.js
+++ client/components/login.js
@@ -7,11 +7,25 @@
 	loginForm.onsubmit = (e) => {
 		e.preventDefault();
 		const email = document.getElementById('login-email').value;
-		const password = document.getElementById('login-password').value;		
-		verifyUser(email, password);
+		const password = document.getElementById('login-password').value;
+		
+		let userData = {};
+		userData.email = email;
+		userData.password = password;
+
+		verifyUser(userData);
 	}
 
-	function verifyUser(email, password) {
-		// ajax request to api
+	function verifyUser(userData) {
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if(xhr.status === 200 && xhr.readyState === 4) {
+				console.log(JSON.parse(xhr.responseText));
+				// redirect with user data
+			}			
+		}
+		xhr.open('POST', '/login');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(userData));
 	}
 })();
--- client/components/signup.js
+++ client/components/signup.js
@@ -24,7 +24,15 @@
 	}
 
 	function createUser(userData) {
-		console.log(userData)
-		// ajax request to api
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if(xhr.status === 200 && xhr.readyState === 4) {
+				console.log(JSON.parse(xhr.responseText));
+				// redirect with user data
+			}			
+		}
+		xhr.open('POST', '/signup');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(userData));
 	}
 })();
