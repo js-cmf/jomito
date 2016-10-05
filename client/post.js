bcdc59aca401de566a4f6da05dce8d71011d94c6
Todd Chisom <hello@toddchisom.me>
2016-10-05 13:11:57 -0700

    Simple view 1 and view all pages along with ajax call for data

--- client/post.js
+++ client/post.js
@@ -1,39 +1,39 @@
-// signup.js
+// post.js
 (() => {
 	'use strict'
 
-	const postData = document.getElementById('post-data');
+	// const postData = document.getElementById('post-data');
 	
-	postData.onsubmit = (e) => {
-		e.preventDefault();
-		const title = document.getElementById('post-title').value;
-		const body = document.getElementById('post-body').value;
-		const status = document.getElementById('post-status-display').textContent;
-		const type = document.getElementById('post_type').value;
-		const user = 'Mario';
-		const imgUrl = [];
-		const videoEmbed = [];
-		// const publishDate = document.getElementById().value;
-		console.log('we executed')
-		if (title !== '' && body !== '') {
-			let data = {};
-			data.title = title;
-			data.body = body;
-			data.status = status;
-			data.post_type = type;
-			data.user_id = user;
-				console.log(data)
-			createPost(data);
-		} else {
-			alert('title and post required');
-		}
-	}
+	// postData.onsubmit = (e) => {
+	// 	e.preventDefault();
+	// 	const title = document.getElementById('post-title').value;
+	// 	const body = document.getElementById('post-body').value;
+	// 	const status = document.getElementById('post-status-display').textContent;
+	// 	const type = document.getElementById('post_type').value;
+	// 	const user = 'Mario';
+	// 	const imgUrl = [];
+	// 	const videoEmbed = [];
+	// 	// const publishDate = document.getElementById().value;
+	// 	console.log('we executed')
+	// 	if (title !== '' && body !== '') {
+	// 		let data = {};
+	// 		data.title = title;
+	// 		data.body = body;
+	// 		data.status = status;
+	// 		data.post_type = type;
+	// 		data.user_id = user; 
+	// 			console.log(data)
+	// 		createPost(data);
+	// 	} else {
+	// 		alert('title and post required');
+	// 	}
+	// }
 
 	function createPost(data) {
 		const req = new XMLHttpRequest();
 		req.onreadystatechange = () => {
 			if (req.status === 200 && req.readyState === 4) {
-				console.log(JSON.parse(req.responseText));
+				console.log(req.responseText);
 				// redirect with user data
 			}
 		}
@@ -41,6 +41,32 @@
 		req.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 		req.send(JSON.stringify(data));
 	}
+	const a = document.getElementById('test-btn');
+        // NOTE: showAlert(); or showAlert(param); will NOT work here.
+        // Must be a reference to a function name, not a function call.
+        a.onclick = getAllPost;
+	function getAllPost() {
+		console.log('in get post')
+		var req = new XMLHttpRequest();
+		req.open('GET', '/api/posts', true);
+		req.onload = function() {
+			if (req.status >= 200 && req.status < 400) {
+				// Success!
+				var data = JSON.parse(req.responseText);
+				console.log(data);
+			} else {
+				// We reached our target server, but it returned an error
+				console.log('error dawg')
+			}
+		};
 
-})();  
+		req.onerror = function() {
+			// There was a connection error of some sort
+		};
+
+		req.send();
+	}
+
+})();
 console.log('we loaded')
+
