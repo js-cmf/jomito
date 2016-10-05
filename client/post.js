b1eb6c07a8d1281cd9b9790ac54dda4051cb621f
Todd Chisom <hello@toddchisom.me>
2016-10-04 19:15:15 -0700

    Posting a message

--- client/post.js
+++ client/post.js
@@ -14,17 +14,33 @@
 		const imgUrl = [];
 		const videoEmbed = [];
 		// const publishDate = document.getElementById().value;
-
+		console.log('we executed')
+		if (title !== '' && body !== '') {
+			let data = {};
+			data.title = title;
+			data.body = body;
+			data.status = status;
+			data.post_type = type;
+			data.user_id = user;
+				console.log(data)
+			createPost(data);
+		} else {
+			alert('title and post required');
+		}
 	}
 
-	const request = new XMLHttpRequest();
-	request.open('POST', '/my/url', true);
-	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-	request.send(data);
-
-	const data = {};
-
-
-
+	function createPost(data) {
+		const req = new XMLHttpRequest();
+		req.onreadystatechange = () => {
+			if (req.status === 200 && req.readyState === 4) {
+				console.log(JSON.parse(req.responseText));
+				// redirect with user data
+			}
+		}
+		req.open('POST', '/api/post', true);
+		req.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+		req.send(JSON.stringify(data));
+	}
 
 })();  
+console.log('we loaded')
