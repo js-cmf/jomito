d551cb31c4c58baae4fda6e79a736cf8f7aa268d
Mike McCready <mikem@inspero.net>
2016-10-05 19:31:54 -0700

    add property types to create collections component, set up post request to create collection

--- client/components/create-collection.js
+++ client/components/create-collection.js
@@ -8,13 +8,43 @@
 
 	function addCollectionProperty(e) {
 		e.preventDefault();
-		let newProp = document.createElement('input');
-		createCollectForm.insertBefore(newProp, addPropertyBtn);
+		let newPropElem = document.createElement('fieldset');
+		newPropElem.innerHTML = '<input type="text" name="collection-property" placeholder="Collection property" /><select class="collection-property-type"><option value="string">text</option><option value="number">number</option><option value="boolean">boolean</option></select>';
+		createCollectForm.insertBefore(newPropElem, addPropertyBtn);
+	}
+
+	function gatherCollectionData() {
+		let collectionData = {};
+		const propertiesArr = createCollectForm.getElementsByTagName('fieldset');
+		const collectionTitle = document.getElementById('collection-title').value;
+		let collectionProperties = {};
+
+		for (let i = 0; i < propertiesArr.length; i++) {
+			let propertyName = propertiesArr[i].getElementsByTagName('input')[0].value;
+			let propertyType = propertiesArr[i].getElementsByTagName('select')[0].value;
+			collectionProperties[propertyName] = propertyType;
+		}
+
+		collectionData.title = collectionTitle;
+		collectionData.properties = collectionProperties;
+		return collectionData;
 	}
 
 	function submitCollection(e) {
 		e.preventDefault();
-		console.log('submitting collection')
+		let collectionData = gatherCollectionData();
+		console.log(collectionData)
+
+		let xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = () => {
+			if(xhr.status === 200 && xhr.readyState === 4) {
+				console.log(JSON.parse(xhr.responseText));
+				// redirect with user data
+			}			
+		}
+		xhr.open('POST', 'api/collections');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(collectionData));
 	}
 
 	(function createEventListeners() {
