707ce2e3fa7fc8b92abfcac486f670b320bb5730
Todd Chisom <hello@toddchisom.me>
2016-10-06 20:35:01 -0700

    can view a single hard coded post

--- client/post.js
+++ client/post.js
@@ -41,12 +41,11 @@
 		req.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 		req.send(JSON.stringify(data));
 	}
-	// const a = document.getElementById('test-btn');
-	// a.onclick = getAllPost;
+
 	const posts = document.getElementById('post-list');
 	// console.log(posts)
 	function getAllPost() {
-		console.log('in get post')
+		console.log('in get all post')
 		var req = new XMLHttpRequest();
 		req.open('GET', '/api/posts', true);
 		req.onload = function() {
@@ -74,8 +73,54 @@
 	};
 	getAllPost();
 	// function 	
+	const a = document.getElementById('get-post');
+	a.onclick = getSinglePost;
 	function getSinglePost() {
-		
+		console.log('in get single post')
+		var req = new XMLHttpRequest();
+		req.open('GET', '/api/post/57f403a10fa83d2b05d7164d', true);
+		req.onload = function() {
+			if (req.status >= 200 && req.status < 400) {
+				// Success!
+				var data = JSON.parse(req.responseText);
+				console.log(data);
+				
+				const title_container = document.getElementById('title');
+				let title = document.createElement("h2");
+				title.textContent = data.title;
+				title_container.appendChild(title);
+
+				const body_container = document.getElementById('body');
+				let body = document.createElement("p");
+				body.textContent = data.body;
+				body_container.appendChild(body);
+
+				const type_container = document.getElementById('post-type');
+				let type = document.createElement("li");
+				type.textContent = data.post_type[0];
+				type_container.appendChild(type);
+
+				const status_container = document.getElementById('post-status');
+				let status = document.createElement("li");
+				status.textContent = data.status;
+				status_container.appendChild(status);
+				// data.forEach(function (item) {
+				// 	let list = document.createElement("li");
+				// 	list.textContent = item.title;
+				// 	posts.appendChild(list);
+				// 	console.log(list);
+				// });
+			} else {
+				// We reached our target server, but it returned an error
+				console.log('error dawg')
+			}
+		};
+
+		req.onerror = function() {
+			// There was a connection error of some sort
+		};
+
+		req.send();
 	};
 
 	
