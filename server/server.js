30e797b3bf52bfb0f61c175e21bf05a2480fda2f
Mike McCready <mikem@inspero.net>
2016-10-07 23:39:17 -0700

    deleting collection data

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ mongoose.connect(dbConfig.url, function(err) {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
